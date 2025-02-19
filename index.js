f1e0e90c6de462c483fb9775adf1f6127da21751
Tizian Siegwardt <siegwardtt9@gmail.com>
2025-02-19 10:48:06 +0100

    clean up

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-import { Client, GatewayIntentBits, Collection, ActivityType, Events } from 'discord.js';
+import { Client, GatewayIntentBits, Collection, Events } from 'discord.js';
 import dotenv from 'dotenv';
-import { botConfig } from './src/config/bot-config.js';
 import { onReady } from './src/listeners/readyListener.js';
-import { loadCommands, handleCommandInteraction } from './src/listeners/commandHandler.js';
+import { handleCommandInteraction } from './src/listeners/commandHandler.js';
 import { setLanguage } from './src/utils/translationHandler.js';
-import { createOrFetchTicketMessage } from './src/server-build/serverSetup.js';
-import { setLoading } from './src/utils/loader.js'
 
 dotenv.config();
 
 const botLanguage = process.env.LANGUAGE || 'default';
 setLanguage(botLanguage);
-
-setLoading(true, "⚙️ Starte Bot...");
 
 const client = new Client({
     intents: [
@@ -34,8 +29,6 @@ client.once(Events.ClientReady, async () => {
 client.on(Events.InteractionCreate, async (interaction) => {
     await handleCommandInteraction(client, interaction);
 });
-
-setLoading(false)
 
 client.login(process.env.DISCORD_BOT_TOKEN).catch((err) => {
     console.error('❌ Fehler beim Login:', err);
